774e9c5e1460a10acbf08a545cdcd80d7a45d0d7
Julie Dedieu <julie.dedieu1@gmail.com>
2026-03-13 09:22:50 +0100

    Print string

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,6 +1,4 @@
 void main() {
   String str = "Holberton School";
-  var new_str = str * 3;
-  var subStr = str.substring(0,9);
-  print('$new_str\n$subStr\n');
+  print('${str * 3}\n${str.substring(0,9)}\n');
 }
