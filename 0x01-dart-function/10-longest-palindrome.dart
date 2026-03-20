09da51bdebf97df56f55aae6335554e0f485cd0a
Julie Dedieu <julie.dedieu1@gmail.com>
2026-03-20 10:37:37 +0100

    Named variables more explicitly

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -3,28 +3,28 @@ import '9-palindrome.dart';
 String longestPalindrome(String s) {
   int stringLen = s.length;
 
-  List<List<bool>> dpTable = List.generate(
+  List<List<bool>> isPalindromeTable = List.generate(
     stringLen,
     (rowIndex) => List.filled(stringLen, false),
   );
 
-  int start = 0;
-  int maxLen = 1;
+  int longestPalindromeStart = 0;
+  int longestPalindromeLength = 1;
 
   // All substrings of length 1 are palindromes
   for (int i = 0; i < stringLen; i++) {
-    dpTable[i][i] = true;
+    isPalindromeTable[i][i] = true;
   }
 
   // Check for substrings of length 2
   for (int i = 0; i < stringLen - 1; i++) {
     // Compare 2 characters
     if (s[i] == s[i + 1]) {
-      dpTable[i][i + 1] = true;
+      isPalindromeTable[i][i + 1] = true;
       // If current longest palindrome length is less than 2, update the start index and the maxLen
-      if (maxLen < 2) {
-        start = i;
-        maxLen = 2;
+      if (longestPalindromeLength < 2) {
+        longestPalindromeStart = i;
+        longestPalindromeLength = 2;
       }
     }
   }
@@ -36,8 +36,8 @@ String longestPalindrome(String s) {
   // the result for its inner substring (length K-2) is ALREADY computed and stored.
   for (int k = 3; k <= stringLen; k++) {
     // Iterate over all substrings from start to end of substring of length k
-    int substringsLengths = stringLen - k + 1;
-    for (int i = 0; i < substringsLengths; i++) {
+    int substringLength = stringLen - k + 1;
+    for (int i = 0; i < substringLength; i++) {
       int substringEndIndex = i + k - 1;
 
       // A substring s[i...end] is a palindrome IF:
@@ -46,22 +46,22 @@ String longestPalindrome(String s) {
       //
       // We do NOT need to re-scan the inside. The dpTable value acts as a "certificate"
       // that all inner layers were valid in previous iterations.
-      if (dpTable[i + 1][substringEndIndex - 1] &&
+      if (isPalindromeTable[i + 1][substringEndIndex - 1] &&
           s[i] == s[substringEndIndex]) {
-        dpTable[i][substringEndIndex] = true;
+        isPalindromeTable[i][substringEndIndex] = true;
 
         // Update global maximum if this new palindrome is longer
-        if (k > maxLen) {
-          start = i;
-          maxLen = k;
+        if (k > longestPalindromeLength) {
+          longestPalindromeStart = i;
+          longestPalindromeLength = k;
         }
       }
     }
   }
   // Requirement: Palindromes must be at least 3 characters long.
   // If the longest found is less than 3 (covers both short inputs and lack of palindromes), return "none".
-  if (maxLen < 3) {
+  if (longestPalindromeLength < 3) {
     return "none";
   }
-  return s.substring(start, start + maxLen);
+  return s.substring(longestPalindromeStart, longestPalindromeStart + longestPalindromeLength);
 }
