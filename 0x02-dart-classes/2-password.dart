59c92ffe3565f8740c62ed2eb311b1eb296a0982
Julie Dedieu <julie.dedieu1@gmail.com>
2026-03-20 14:41:02 +0100

    Password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,20 @@
+class Password {
+  String password = "";
+
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+    if (!password.contains(RegExp(r'[A-Z]')) 
+        || !password.contains(RegExp(r'[a-z]'))
+        || !password.contains(RegExp(r'\d+'))
+        ) {
+          return false;
+        }
+    return true;
+  }
+
+  String toString() {
+    return ('Your Password is: $password');
+  }
+}
