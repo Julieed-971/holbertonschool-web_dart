c65c3bc539cf591b898baa0a148c82177c6f21c5
Julie Dedieu <julie.dedieu1@gmail.com>
2026-03-20 15:42:56 +0100

    Json to User

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,32 @@
+class User {
+  int id = 0;
+  String name = "";
+  int age = 0;
+  double height = 0;
+
+  User({required this.id, required this.name, required this.age, required  this.height}) {
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+  
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    User newUser = new User(
+      id: userJson["id"], 
+      name: userJson["name"],
+      age: userJson["age"],
+      height: userJson["height"]
+      );
+    return newUser;
+  }
+
+  String toString() {
+    return ('User(id: $id, name: $name, age: $age, height: $height)');
+  }
+}
