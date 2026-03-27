807a088eb2cbdb2ac15a40b52ae2675782861cec
Julie Dedieu <julie.dedieu1@gmail.com>
2026-03-27 09:54:09 +0100

    Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import '4-util.dart';
+
+Future<double> calculateTotal() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> userObject = jsonDecode(userData);
+    String userId = userObject["id"];
+
+    String userOrdersData = await fetchUserOrders(userId);
+
+    List userProducts = jsonDecode(userOrdersData);
+
+    double totalPrice = 0;
+
+    for (int i = 0; i < userProducts.length; i++) {
+      String productPrice = await fetchProductPrice(userProducts[i]);
+      double price = double.parse(productPrice);
+      totalPrice += price;
+    }
+    return totalPrice;
+  } catch (error) {
+    return -1;
+  }
+}
